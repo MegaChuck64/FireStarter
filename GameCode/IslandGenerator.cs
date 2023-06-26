fbdc5a5d99a35b4423ef57127e9fd501ca9a582b
MegaChuck64 <cjscottcarey@yahoo.com>
2023-06-26 00:57:16 -0400

    got the edges of the island working

--- GameCode/IslandGenerator.cs
+++ GameCode/IslandGenerator.cs
@@ -12,7 +12,43 @@ public class IslandGenerator
         {
             for (int y = 0; y < size.Y; y++)
             {
-                game.GameObjects.Add(new Tile(game)
+                var spriteName = "grass";
+
+
+                if (x == 0 && y == 0)
+                {
+                    spriteName = "shore_top_left";
+                }
+                else if (x == size.X - 1 && y == 0)
+                {
+                    spriteName = "shore_top_right";
+                }
+                else if (x == 0 && y == size.Y - 1)
+                {
+                    spriteName = "shore_bottom_left";
+                }
+                else if (x == size.X - 1 && y == size.Y - 1)
+                {
+                    spriteName = "shore_bottom_right";
+                }
+                else if (x == size.X - 1)
+                {
+                    spriteName = "shore_right";
+                }
+                else if (y == size.X - 1)
+                {
+                    spriteName = "shore_bottom";
+                }
+                else if (y == 0)
+                {
+                    spriteName = "shore_top";
+                }
+                else if (x == 0)
+                {
+                    spriteName = "shore_left";
+                }
+
+                game.GameObjects.Add(new Tile(game, spriteName)
                 {
                     Transform = new Transform(new Point(x * 32,y * 32), Vector2.One, 0f)
                 });                
@@ -26,9 +62,9 @@ public class Tile : GameObject
     public override Rectangle Bounds =>
         new(Transform.Position, new Point(32, 32));
 
-    public Tile(MainGame game) : base(game)
+    public Tile(MainGame game, string sprite) : base(game)
     {
-        var txt = game.Content.Load<Texture2D>(@"Sprites\grass");
+        var txt = game.Content.Load<Texture2D>(@"Sprites\" + sprite);
         Component.Add(new Sprite(this, txt, Color.White));
         
     }
